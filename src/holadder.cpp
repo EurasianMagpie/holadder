fed75c27ec557bd113e826c8bc962783728de141
EurasianMagpie <akkala@163.com>
2023-04-05 15:42:48 +0800

    feature, batch config

--- src/holadder.cpp
+++ src/holadder.cpp
@@ -12,7 +12,7 @@ void applyUfwRuleForNewPort(int portFrom, int portTo) {
     std::system(szCmd);
 }
 
-bool modifyLadderConfByServiceName(const char* serviceName) {
+bool modifyLadderConfByServiceName(const char* serviceName, short expectedPort) {
     char szFilePath[1024] = {};
     sprintf(szFilePath, "/etc/openvpn/%s.conf", serviceName);
     std::cout << "[-] modifyLadderConf:" << serviceName << " File: " << szFilePath << std::endl;
@@ -24,18 +24,36 @@ bool modifyLadderConfByServiceName(const char* serviceName) {
     std::string v;
     conf.get("port", v);
     int port = std::atoi(v.c_str());
-    if (port < 3000 || port > 65353) {
-        port = 3000;
-    }
+
     char szPort[16] = {};
-    sprintf(szPort, "%d", port + 1);
+    sprintf(szPort, "%d", expectedPort);
     v = szPort;
     conf.set("port", v);
-    applyUfwRuleForNewPort(port, port + 1);
+    applyUfwRuleForNewPort(port, expectedPort);
 
-    std::cout << "    port\t" << port << " -> " << szPort << std::endl;
+    std::cout << "    port\t" << port << " -> " << expectedPort << std::endl;
 
     return conf.save(szFilePath);
+}
+
+short getBasePortFromServiceName(const char* serviceName) {
+    char szFilePath[1024] = {};
+    sprintf(szFilePath, "/etc/openvpn/%s.conf", serviceName);
+    std::cout << "[-] get base port from :" << serviceName << " File: " << szFilePath << std::endl;
+    LadderConfUtil::LadderConf conf;
+    if (!conf.parse(szFilePath)) {
+        std::cout << "    invalid conf file or path > " << szFilePath << std::endl;
+        return false;
+    }
+    std::string v;
+    conf.get("port", v);
+    int port = std::atoi(v.c_str());
+    if (port < 9000 || port > 65353) {
+        return 9000;
+    }
+    else {
+        return port;
+    }
 }
 
 void restartUfw() {
@@ -49,24 +67,38 @@ void restartService(const char* serviceName) {
     std::system(szCmd);
 }
 
+
+const char* usage = "    usage: holadder [service name] [batch count]";
+
 int main(int argc, char *argv[])
 {
     std::cout << "Hello Holadder." << std::endl;
     const char* serviceName = nullptr;
-    if (argc == 1) {
-        std::cout << "    usage: holadder [openvpn service name]" << std::endl;
-        return 0;
-    }
-    else {
-        serviceName = argv[1];
-    }
-
-    if (!modifyLadderConfByServiceName(serviceName)) {
+    int batchCount = 1;
+    if (argc < 3) {
+        std::cout << usage << std::endl;
         return 0;
     }
 
+    serviceName = argv[1];
+    batchCount = atoi(argv[2]);
+    if (batchCount < 1) {
+        std::cout << usage << std::endl;
+        return 0;
+    }
+
+    short basePort = 0;
+    for (int i = 0; i < batchCount; i++) {
+        char szServiceName[16] = {};
+        sprintf(szServiceName, "%s%d", serviceName, i);
+
+        if (basePort = 0) {
+            basePort = getBasePortFromServiceName(szServiceName) + batchCount;
+        }
+
+        modifyLadderConfByServiceName(serviceName, basePort++);
+        restartService(serviceName);
+    }
     restartUfw();
-    restartService(serviceName);
-
     return 0;
 }
